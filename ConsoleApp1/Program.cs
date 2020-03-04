fb7511a0311505d1f3bd05bce14cb4a8dae4c285
unknown <diegog1307@gmail.com>
2020-03-04 11:41:36 -0300

    github

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] a = { "1", "2", "3" };
+
+            foreach( string nome in a)
+            {
+                Console.WriteLine(nome);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
